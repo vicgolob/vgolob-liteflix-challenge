5d8029e9711ada4352ef1bde4dc7e4133eb793e9
Victor Golob <victorgolob@gmail.com>
2023-09-18 16:07:39 -0300

    FE - create FE react app with Tailwind

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        React App is running with Tailwind styles
+      </h1>
+    </div>
+  );
+}
+
+export default App;
