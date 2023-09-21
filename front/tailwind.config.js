6845221ae2152fdfc7eda7e7ba9995cabed94de4
Victor Golob <victorgolob@gmail.com>
2023-09-21 11:07:31 -0300

    tailwind config fonts and colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bold: ['BebasNeue-Bold', 'sans-serif'],
+        regular: ['BebasNeue-Regular', 'sans-serif'],
+        light: ['BebasNeue-Light', 'sans-serif'],
+      },
+      colors: {
+        black: '#242424',
+      },
+    },
+  },
+  plugins: [],
 };
