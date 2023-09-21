26900c38fa897f4192c3ccd79ca74fbfe6227441
Victor Golob <victorgolob@gmail.com>
2023-09-21 11:06:02 -0300

    add NavBar component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavBar from '@components/NavBar';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        React App is running with Tailwind styles
-      </h1>
-    </div>
+    <Router>
+      <main
+        className="relative md:bg-black/90 h-screen px-6 md:px-20 pt-5 md:pt-7 pb-14"
+        style={{
+          background:
+            'url(https://image.tmdb.org/t/p/original/ctMserH8g2SeOAnCw5gFjdQF8mo.jpg) 10% / cover no-repeat',
+        }}>
+        <NavBar />
+      </main>
+    </Router>
   );
 }
 
