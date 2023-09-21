a2dc319d6cff0d0e10e8df741522d960988b17a2
Victor Golob <victorgolob@gmail.com>
2023-09-21 11:05:11 -0300

    add constants

---
+++ front/src/constants/navLinks.tsx
@@ -0,0 +1,14 @@
+export interface INavLink {
+  title: string;
+  href: string;
+}
+
+export const NAV_LINKS = [
+  { title: 'inicio', href: '#' },
+  { title: 'series', href: '#' },
+  { title: 'películas', href: '#' },
+  { title: 'agragadas recientemente', href: '#' },
+  { title: 'populares', href: '#' },
+  { title: 'mis películas', href: '#' },
+  { title: 'mi lista', href: '#' },
+];
