7d03bdece92aab19bdc546692a4bb8f16d009f41
Victor Golob <victorgolob@gmail.com>
2023-09-28 02:18:48 -0300

    FE - update interfaces

--- front/src/interfaces/index.ts
+++ front/src/interfaces/index.ts
@@ -1,17 +1,28 @@
-export interface IMovie {
+export interface MovieData {
+  id: number;
   backdrop_path: string;
   title: string;
-  vote_average: number;
-  release_date: string;
+  vote_average?: number;
+  release_date?: string;
+}
+
+export interface FetchMoviesResponse {
+  page: number;
+  results: MovieData[];
+  total_pages: number;
+  total_results: number;
 }
 
 export interface SidePanelContextProps {
   sidePanelIsOpen: boolean;
   toggleSidePanelIsOpen: Function;
-  sidePanelIsMobile: boolean;
-  setSidePanelIsMobile: Function;
 }
 
 export interface SidePanelRefType {
   forceClosePanel: () => void;
+}
+
+export interface DropdownMenuOption {
+  label: string;
+  value: string;
 }
